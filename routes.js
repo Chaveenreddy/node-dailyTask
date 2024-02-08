13043a35ba0ef02f69cea57d09fd102bd02b4a98
Chaveenreddy <159451683+Chaveenreddy@users.noreply.github.com>
2024-02-08 20:39:59 +0530

    Add files via upload
    Expoting of files to other files

---
+++ routes.js
@@ -0,0 +1,40 @@
+
+
+const fs = require("fs");
+
+const reqResHandler = (req, res) => {
+    const method = req.method;
+    const url =req.url;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>responce</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name ="message"/><button type ="submit">send</button></form> </body>')
+        res.write('/</html>')
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunck) => {
+            console.log(chunck); body.push(chunck);
+        })
+        //event driven architecture 
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+
+            fs.writeFile('message.txt', message, (e) => {
+                res.statusCode = 302;
+                res.setHeader('location', "/");
+                return res.end();
+            });
+        })
+    }
+}
+//one method  of export
+module.exports =reqResHandler;
+//another type of export
+// module.exports={
+
+//     handler: reqResHandler,
+//     text: "this is dummy text"
+// }
